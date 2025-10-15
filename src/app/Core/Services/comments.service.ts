d10e3528aecbc34a578801e892e19c4602839247
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-10-15 21:02:38 +0300

    Comments services

--- src/app/Core/Services/comments.service.ts
+++ src/app/Core/Services/comments.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  constructor(private readonly _HttpClient: HttpClient) { }
+  CreateComment(data: object): Observable<any> {
+    return this._HttpClient.post(`${environment.baseURL}/comments`, data);
+  }
+  GetPostComment(postId: string): Observable<any> {
+    return this._HttpClient.get(`${environment.baseURL}/posts/${postId}/comments`)
+  }
+  UpdatedPostComment(commentId: string, data: object): Observable<any> {
+    return this._HttpClient.put(`${environment.baseURL}/comments${commentId}`, data)
+  }
+  DeleteComment(commentId:string):Observable<any> {
+    return this._HttpClient.delete(`${environment.baseURL}/comments${commentId}`)
+  }
 }
